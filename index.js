171c4aade178f2434b44344f93bfd2c308f27d0a
unknown <afifmakarim88@gmail.com>
2022-12-24 17:58:31 +0700

    fixing

--- index.js
+++ index.js
@@ -28,13 +28,13 @@ const fetchTwitterAva = async (twitterUsername) => {
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
-  const navigationPromise = page.waitForNavigation({
-    waitUntil: "domcontentloaded",
-  });
+  // const navigationPromise = page.waitForNavigation({
+  //   waitUntil: "domcontentloaded",
+  // });
   // await page.setDefaultNavigationTimeout(300000);
-  await navigationPromise;
+  // await navigationPromise;
   await page.goto(`https://twitter.com/${twitterUsername}`);
-  await navigationPromise;
+  // await navigationPromise;
   await page.waitForSelector('a[href$="/photo"] img[src]');
   const url = await page.evaluate(
     () => document.querySelector('a[href$="/photo"] img').src
